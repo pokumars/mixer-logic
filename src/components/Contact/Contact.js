35deacfb12b2074579c3e617a2dcc522b1721974
oheneba <ohenebakob@hotmail.com>
2020-07-03 16:17:27 +0300

    make a contact me page that also has your CV

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,21 +6,24 @@ const Contact = () => {
     <div className="contact">
       <h2 className="my-name">Oheneba Poku-Marboah</h2>
       <div className="socials">
-        <a href="https://www.linkedin.com/in/oheneba-poku-marboah-483783173/">
-          <img alt="linkedin" src={require('../../graphics/linkedin-icon.svg')}/>
+        <a href="https://www.linkedin.com/in/oheneba-poku-marboah-483783173/" className="contact-element">
+          <img alt="linkedin" src={require('../../graphics/icons/linkedin-icon.svg')}/>
           <strong>LinkedIn</strong>
         </a>
-        <a href="https://github.com/pokumars">
-          <img alt="github" src={require('../../graphics/github-icon.svg')}/>
+        <a href="https://github.com/pokumars" className="contact-element">
+          <img alt="github" src={require('../../graphics/icons/github-icon.svg')}/>
           <strong>GitHub</strong>
         </a>
       </div>
-      <div className="my-email">
-        <img alt="cv" src={require('../../graphics/email-icon.svg')}/>
-        <p>ohenebakob@yahoo.com</p>
+      <div className="my-email contact-element">
+        <img alt="cv" src={require('../../graphics/icons/email-icon.svg')}/>
+        <p>Shoot me some feedback @ ohenebakob@yahoo.com</p>
       </div>
-      <div className="my-cv">
-        <img alt="cv" src={require('../../graphics/cv-icon.svg')}/>
+      <div className="my-cv contact-element">
+        <img alt="cv" src={require('../../graphics/icons/cv-icon.svg')}/>
+        <p><a href={require('../../graphics/cv_oheneba_pokumarboah.pdf')} target="_blank" rel="noopener nonreferrer">
+          my Curriculum Vitae
+        </a></p>
       </div>
       
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ export const Navbar = () => {
       <div className="logo-container">
         <a href="/">
           <img className="logo" data-testid="logo" alt="cocktail glass"
-           src={require('../../graphics/cocktail-light-green.svg')} />
+           src={require('../../graphics/icons/cocktail-light-green.svg')} />
           <span><b>mixer logic</b> </span>
         </a>
       </div>
