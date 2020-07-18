163f090682b679d7565e9bb943953a21bb5e208f
oheneba <ohenebakob@hotmail.com>
2020-07-18 14:28:44 +0300

    Make tests for searchReducer -> FIND_BY_NAME function

--- src/reducers/searchReducer.test.js
+++ src/reducers/searchReducer.test.js
@@ -1,11 +1,12 @@
-import { findDrinksByName, findDrinksByMethod, findDrinksByAlcohol, emptyResultsText,
+import searchReducer, { findDrinksByName, findDrinksByMethod, findDrinksByAlcohol, emptyResultsText,
   FIND_BY_NAME, FIND_BY_ALCOHOL, FIND_BY_METHOD } from "./searchReducer";
+import reducer from "./searchReducer";
 
 
 // ---------------------ACTION CREATORS-----------------
 // It is easy to search for and get only the test objects since they have 
 // very distinct names from the rest of the objects
-const  testSingleResult=  {
+const  testSingleResult=  [{
   name: "test DrinkName",
   ingredients: [
     ["testIngredientName",4, "unit"],
@@ -18,6 +19,7 @@ const  testSingleResult=  {
   method: ["testMethod1","testMethod2"],
   garnish: ["testGarnish1","testGarnish2"],
   categories: ["testCategory1", "testCategory2"],
+  alcohols: ['testAlcohol1', 'testAlcohol2'],
   page: 1,
   credits:[["testEntity1", "xforWhatImg"], ["testEntity2", "xforWhat"]],
   steps: [
@@ -27,10 +29,9 @@ const  testSingleResult=  {
    "testStep4",
    "testStep5"
   ]    
-}
+}]
 
 const testMultipleResult = [
-  testSingleResult,
   {
     name: "drinkName",
     ingredients: [
@@ -44,6 +45,7 @@ const testMultipleResult = [
     method: ["method1","method2"],
     garnish: ["garnish1","garnish2"],
     categories: ["category1", "category2"],
+    alcohols: ['alcohol1', 'alcohol2'],
     page: 1,
     credits:[["entity1", "forWhatImg"], ["entity1", "forWhat"]],
     steps: [
@@ -53,7 +55,8 @@ const testMultipleResult = [
      "step4",
      "step5"
     ]    
-  } 
+  },
+  ...testSingleResult
 ]
 describe('action creator tests', () => {
   const testQuery = "test"
@@ -91,46 +94,64 @@ describe('action creator tests', () => {
   })
 })
 
+
 // ---------------------REDUCER TESTS-----------------
-describe.skip('FIND_BY_NAME action creator tests', () => {
-
-  it('should find one drink by name', () => {
-    const res = [testSingleResult]
-    const empty = emptyResultsText(res)
-
-    const expectedAction = {
-      type: FIND_BY_NAME,
-      data: { res , empty }
-    }
-    expect(findDrinksByName("test")).toBeDefined()
-    expect(findDrinksByName("test")).toEqual(expectedAction)
+describe('FIND_BY_NAME reducer tests', () => {
+  test('undefined state returns the initial state', () => {
+    const theReducer = searchReducer(undefined, {})
+    expect(theReducer).toBeDefined()
+    expect(theReducer).toEqual({ res: [], empty: '' })
     
   })
 
+  
+  test('state returns only 1 drink by name', () => {
+    const theReducer = searchReducer([], {
+      type: FIND_BY_NAME,
+      query: "test"
+    }) //returns an object with keys res:[{},{}] and empty:''
+    console.log(theReducer)
+    const res = testSingleResult
+    const empty = emptyResultsText(res)
+
+    expect(theReducer).toBeDefined()
+    expect(theReducer).toEqual({ res: testSingleResult, empty: empty })
+    //the results I get is a subset of the results I expect to get
+    expect(theReducer.res).toEqual(expect.arrayContaining(testSingleResult))
+  })
+
   it('should find multiple drinks by name', () => {
+    const theReducer = searchReducer([], {
+      type: FIND_BY_NAME,
+      query: "drinkName"
+    })//returns an object with keys res:[{},{}] and empty:''
+
     const res = testMultipleResult
     const empty = emptyResultsText(res)
 
-    const expectedAction = {
-      type: FIND_BY_NAME,
-      data: { res , empty }
-    }
-    expect(findDrinksByName("drinkName")).toBeDefined()
+    expect(theReducer).toBeDefined()
+    expect(theReducer).toEqual({ res, empty })
     //the results I get is a subset of the results I expect to get
-    expect(findDrinksByName("drinkName").data.res).toEqual(expect.arrayContaining(testMultipleResult))
+    expect(theReducer.res).toEqual(expect.arrayContaining(testMultipleResult))
     //if the expected drinks are a subset of the received, verify that the received contains only 2.
-    expect(findDrinksByName("drinkName").data.res).toHaveLength(2)
+    expect(theReducer.res).toHaveLength(2)
   })
 
   it('should find 0 drink by name', () => {
+    const theReducer = searchReducer([], {
+      type: FIND_BY_NAME,
+      query: "xg"
+    })//returns an object with keys res:[{},{}] and empty:''
+
     const res = []
     const empty = emptyResultsText(res)
 
-    const expectedAction = {
-      type: FIND_BY_NAME,
-      data: { res , empty }
-    }
-    expect(findDrinksByName("xg")).toEqual(expectedAction)
+    expect(theReducer).toBeDefined()
+    expect(theReducer).toEqual({ res, empty })
+    //the results I get is a subset of the results I expect to get
+    expect(theReducer.res).toEqual(expect.arrayContaining([]))
+    //if the expected drinks are a subset of the received, verify that the received contains only 2.
+    expect(theReducer.res).toHaveLength(0)
   })
 })
 
@@ -159,3 +180,6 @@ describe.skip('FIND_BY_METHOD action creator tests', () => {
     expect(findDrinksByMethod("xg")).toEqual(expectedAction)
   })
 })
+
+
+
