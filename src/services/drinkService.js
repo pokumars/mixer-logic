2f6c3c626494c7f39044b10b44abb6eb719062bf
oheneba <ohenebakob@hotmail.com>
2020-09-13 02:55:00 +0300

    create services directory for axios; make drinkservice.js for fetching drinks

---
+++ src/services/drinkService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+const baseURL = 'http://localhost:3001/drinks'
+
+const getAllDrinks = () => {
+  const request = axios.get(`${baseURL}`)
+
+  return request.then((response) => {
+    console.log('axios fetch of drinks fulfilled')
+    return response.data
+  })
+}
+
+const getSingleDrink = (drinkID) => {
+  const request = axios.get(`${baseURL}/${drinkID}`)
+
+  return request.then((response) => {
+    console.log(`axios fetch of single drink fulfilled`)
+    return response.data
+  })
+}
+
+
+export default { getAllDrinks, getSingleDrink }
