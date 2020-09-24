e0a8772a2a9700b75c723f6c1c9485d981f06597
oheneba <ohenebakob@hotmail.com>
2020-09-25 00:18:22 +0300

    add proxy to package.json and change to relative url

--- src/services/drinkService.js
+++ src/services/drinkService.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:3001/drinks'
+//const baseURL = 'http://localhost:3001/api/drinks'
+//const baseURL = 'http://ec2-18-192-55-244.eu-central-1.compute.amazonaws.com:3003/api/drinks'
+// baseUrl as a relative URL because it is hosted from the same root folder
+const baseURL = '/api/drinks'
+
 
 const getAllDrinks = () => {
   const request = axios.get(`${baseURL}`)
